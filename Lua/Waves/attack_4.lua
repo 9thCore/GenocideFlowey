d0cb22834c22aedfa3a59bc698fb24df51d4cd96
9thCore <38700349+9thCore@users.noreply.github.com>
2024-01-26 15:10:33 +0200

    attack 4

--- Lua/Waves/attack_4.lua
+++ Lua/Waves/attack_4.lua
@@ -1,11 +1,18 @@
 attacks = require "attacks"
 f_parry = require "f_parry"
 easing = require "easing"
-Encounter["wavetimer"] = 7
+Encounter["wavetimer"] = 9
 timer = 0
+attacks.CreateLocket()
 
 function Update()
 	attacks.Update()
+
+	if timer > 30 and timer < 60 then
+		attacks.SetLocketAlpha(easing.Out((timer - 30) / 30), 4)
+	elseif timer % 240 == 60 then
+		attacks.LaunchLocket(Player.absx, Player.absy, 120)
+	end
 
 	timer = timer + 1
 end
